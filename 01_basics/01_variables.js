8858a0390839011857d997815333d0b4c3679d98
rupalimarwadi <rupali0103@gmail.com>
2024-02-15 13:10:26 +0530

    dataTypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail="rupali0103@gmail.com"
+var accountPassword="test"
+accountCity="Pune"
+// accountId=2 // not allowed
+accountEmail="rupalimarwadi@gmail.com"
+accountPassword="testtttt222"
+accountCity="Dewas"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(3+3);
+console.log("Rupali");
+
+// Boolean => true/false
+// String
+// Number
+// null
+// undefined 
+// Symbol => unique
+// BigInt 
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof  undefined); //undefined
